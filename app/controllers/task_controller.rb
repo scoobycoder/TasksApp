7d5caa47c284f21d62d4cc2426352ee73d41d07e
Nate <nathan_halterman@gap.com>
2016-07-19 09:09:44 -0400

    more messing with forms, still can't create or load a record

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,15 +1,16 @@
 class TaskController < ApplicationController
 
   def put
-    @task = Task.new
+
   end
 
   def post
     @task = Task.new
+    Task.create(name: 'Mow', complete: false)
   end
 
   def get
-
+    @task = Task.all
   end
 
 end
