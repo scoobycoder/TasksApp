6e13f893f98d692feb8068814f3b57e5d275cb4b
Nate <nathan_halterman@gap.com>
2016-08-02 09:01:41 -0400

    adding back the model for task after running migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
 class TasksController < ApplicationController
   def new
   end
+
+  def create
+    render plain: params[:task].inspect
+
+    @task = Task.new(params[:task])
+
+    @task.save
+    redirect_to @task
+  end
 end
