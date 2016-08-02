143319cfeb40c6dcf4d67f577623f0edea2160c0
Nate <nathan_halterman@gap.com>
2016-08-02 09:11:11 -0400

    attempting to get fulfill security requirement, this is broken

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,14 @@ class TasksController < ApplicationController
   def create
     render plain: params[:task].inspect
 
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
 
     @task.save
     redirect_to @task
   end
+end
+
+private
+def task_params
+  params.require(:task).permit(:task, :complete)
 end
